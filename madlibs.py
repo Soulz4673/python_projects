3244411b5d2198f46f84045ff548d13481b9be00
Soulz4673 <106358327+Soulz4673@users.noreply.github.com>
2022-12-06 23:05:39 -0800

    Add files via upload
    beginners project madlibs

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+#string concatenation
+#suppose we want to create a string that says "subcribe to __"
+#youtuber =""
+#some string variable
+#print("subscribe to "+ youtuber)
+#print("subsctribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+
+madlib = f"Computer programing is so {adj}! It makes me so happy all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
